35fdfed84c2e45e5142f2a9308135a793b069e23
Anastasia <super.okay335@yandex.ru>
2022-08-21 19:04:25 +0300

    Добавили решение задачи в 1 проекте

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int x, y, max, min;
+Console.WriteLine("Введите первое число");
+x = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+y = Convert.ToInt32 (Console.ReadLine());
+max = 0;
+min = 0;
+if (x>y)
+{
+    max = x;
+    min = y;
+}
+else
+{
+    min = x;
+    max = y;
+}
+Console.WriteLine("Большее число "+ (max ));
+Console.WriteLine("Меньшее число "+ (min ));
